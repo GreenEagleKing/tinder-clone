6eb56f9de2298a24cea15722f5d8474befa7fb4c
Ben King <benking1293@gmail.com>
2022-10-07 18:30:08 +0100

    Header created

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./Header.css"
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import IconButton from '@mui/material/IconButton';
 
 function Header() {
     return (
-        <div>
-            <h2>I am a Header</h2>
+        // BEM
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+            <img className="header__logo" src='https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png' alt="tinder logo"></img>
+
+            <IconButton>
+                <ForumIcon className="header__icon"/>
+            </IconButton>
         </div>
     )
 }
