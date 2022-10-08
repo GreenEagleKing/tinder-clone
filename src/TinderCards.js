515b1c9798de58620f920e9197df9801222142c1
Ben King <benking1293@gmail.com>
2022-10-08 11:28:39 +0100

    Card css and stacked cards on top of each other

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TinderCard from 'react-tinder-card';
+import './TinderCards.css';
 
 function TinderCards() {
     const [people, setPeople] = useState([
@@ -17,18 +18,19 @@ function TinderCards() {
     return (
         <div>
             <h1>Tinder Cards</h1>
-
-            {people.map(person => (
-                <TinderCard
-                className='swipe'
-                key={person.name}
-                preventSwipe={['up', 'down']}
-                >
-                    <div style={{backgroundImage: `url(${person.url})`}} className='card'>
-                        <h3>{person.name}</h3>
-                    </div>
-                </TinderCard>
-            ))}
+            <div className='tinderCards__cardContainer'>
+                {people.map(person => (
+                    <TinderCard
+                    className='swipe'
+                    key={person.name}
+                    preventSwipe={['up', 'down']}
+                    >
+                        <div style={{backgroundImage: `url(${person.url})`}} className='card'>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     )
 }
